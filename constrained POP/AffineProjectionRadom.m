268a8845ae8b4035c5e7d8428e16d1ae711494fa
Yang Zheng <zhengy093@gmail.com>
2017-08-13 11:46:54 +0100

    Test of linear projections

--- constrained POP/AffineProjectionRadom.m
+++ constrained POP/AffineProjectionRadom.m
@@ -1,13 +1,13 @@
 
 clear;clc;
-load RandomPoly.mat
+load('RandomPoly.mat','data')
 
 n = length(data);
 
-TimeTotal  = zeros(n,3);    % blk, ldl, blk-ldl
-TimeLinear = zeros(n,3);
-TimeConic  = zeros(n,3);
-Iter       = zeros(n,3);
+TimeTotal  = zeros(n,4);    % blk, ldl, blk-ldl, blk-chol
+TimeLinear = zeros(n,4);
+TimeConic  = zeros(n,4);
+Iter       = zeros(n,4);
 
 opts1.solver = 'sos';
 opts1.maxIter = 200;
@@ -21,17 +21,21 @@ opts3.solver = 'sos';
 opts3.maxIter = 200;
 opts3.KKTfact = 'blk-ldl';
 
+opts4.solver = 'sos';
+opts4.maxIter = 200;
+opts4.KKTfact = 'blk-chol';
 for i = 1:n
     
     [x,y,z,info2] = cdcs(data{i}.At,data{i}.b,data{i}.c,data{i}.K,opts2);
     [x,y,z,info3] = cdcs(data{i}.At,data{i}.b,data{i}.c,data{i}.K,opts3);
+    [x,y,z,info4] = cdcs(data{i}.At,data{i}.b,data{i}.c,data{i}.K,opts4);
     [x,y,z,info1] = cdcs(data{i}.At,data{i}.b,data{i}.c,data{i}.K,opts1);
     
     
-    TimeTotal(i,:) = [info1.time.total,info2.time.total,info3.time.total];
-    TimeLinear(i,:) = [info1.time.subiter(1),info2.time.subiter(1),info3.time.subiter(1)];
-    TimeConic(i,:) = [info1.time.subiter(2),info2.time.subiter(2),info3.time.subiter(2)];
-    Iter(i,:)      = [info1.iter,info2.iter,info3.iter];
+    TimeTotal(i,:) = [info1.time.total,info2.time.total,info3.time.total,info4.time.total];
+    TimeLinear(i,:) = [info1.time.subiter(1),info2.time.subiter(1),info3.time.subiter(1),info4.time.subiter(1)];
+    TimeConic(i,:) = [info1.time.subiter(2),info2.time.subiter(2),info3.time.subiter(2),info4.time.subiter(2)];
+    Iter(i,:)      = [info1.iter,info2.iter,info3.iter,info4.iter];
     
 end
 
